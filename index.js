e49ef911413103efd558df160ed468fa4c5190f2
Anirban <anirban008jana@gmail.com>
2025-09-18 10:23:19 +0530

    created signup and login page

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './config/connectDB.js';
+import cookieParser from 'cookie-parser';
+import authRouter from './route/authRoute.js';
+import cors from "cors"
+
+dotenv.config();
+
+const port  = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}))
+
+app.use("/api/auth",authRouter)
+
+app.get('/', (req, res) => {
+    res.send('Hello from AETHERLEARN ')
+})
+
+app.listen(port,() =>{
+    console.log(`Server is running on port : ${port}`)
+    connectDb()
+})
---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type :String ,
+        required : true
+    },
+    description : {
+        type :String ,
+    },
+    email:{
+        type :String ,
+        required : true,
+        unique : true
+    },
+    password : {
+        type :String ,
+    },
+    role:{
+        type :String ,
+        enum:["student","educator"],
+        required : true
+    },
+    photoUrl:{
+        type :String ,
+        default:""
+    },
+    enrolldCourses:[{
+        type :mongoose.Schema.Types.ObjectId ,
+        ref : "Course"
+    }]
+},{timestamps:true})
+
+
+const User = mongoose.model("User",userSchema)
+export default (User)
