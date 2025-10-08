b289b624c86d2d3ae93312ad22e096f290bd8de7
Anirban <anirban008jana@gmail.com>
2025-10-08 20:53:34 +0530

    edit lecture feature done

--- controller/courseController.js
+++ controller/courseController.js
@@ -1,5 +1,10 @@
 import Course from "../models/courseModel.js";
+import Lecture from "../models/lectureModel.js";
 import uploadOnCludinary from "../config/cloudinary.js";
+
+
+// course 
+
 
 export const createCourse = async (req,res)=>{
     try {
@@ -103,5 +108,99 @@ export const removeCourse = async (req,res)=>{
         return res.status(200).json({ message: `Course removed successfully` })
     } catch (error) {
          return res.status(500).json({ message: `Failed to delete courses ${error}` });
+    }
+}
+
+
+// lecture
+
+
+export const createLecture = async (req, res) => {
+  try {
+    const { lectureTitle } = req.body;
+    const { courseId } = req.params;
+
+    if (!lectureTitle || !courseId) {
+      return res.status(400).json({ message: "Lecture title and course ID are required" });
+    }
+
+    const lecture = await Lecture.create({ lectureTitle });
+
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    course.lectures.push(lecture._id);
+    await course.save();
+
+    await course.populate("lectures");
+
+    return res.status(201).json({ lecture, course });
+  } catch (error) {
+    return res.status(500).json({ message: `Lecture creation failed: ${error.message}` });
+  }
+};
+
+
+export const getCourseLecture = async (req, res) => {
+  try {
+    const { courseId } = req.params;
+
+    const course = await Course.findById(courseId).populate("lectures");
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    await course.populate("lectures")
+    await course.save()
+    return res.status(201).json(course);
+  } catch (error) {
+    return res.status(500).json({ message: `Failed to fetch lectures: ${error.message}` });
+  }
+};
+
+
+export const editLecture = async (req,res) =>{
+    try {
+        const {lectureId} = req.params;
+        const {isPreviewFree , lectureTitle} = req.body
+        const lecture = await Lecture.findById(lectureId)
+        if(!lecture){
+            return res.status(404).json({ message: "Lecture not found" });
+        } 
+
+        let videoUrl
+        if(req.file){
+            videoUrl = await uploadOnCludinary(req.file.path)
+            lecture.videoUrl = videoUrl
+        }
+        if(lectureTitle){
+            lecture.lectureTitle = lectureTitle
+        }
+        lecture.isPreviewFree = isPreviewFree
+
+        await lecture.save()
+        return res.status(200).json(lecture);
+    } catch (error) {
+        return res.status(500).json({ message: `Failed to edit lectures: ${error.message}` });        
+    }
+} 
+
+export const removeLecture = async (req,res)=>{
+    try {
+        const {lectureId} = req.params
+        const lecture = await Lecture.findByIdAndDelete(lectureId)
+
+        if(!lecture){
+            return res.status(404).json({ message: "Lecture not found" });
+        }
+        await Course.updateOne(
+            {lectures : lectureId},
+            {$pull : {lectures : lectureId}}
+        )
+        return res.status(200).json({message : "Lecture removed successfully"});
+    } catch (error) {
+        return res.status(500).json({ message: `Failed to remove lectures: ${error.message}` });
     }
 }
--- route/courseRoute.js
+++ route/courseRoute.js
@@ -1,7 +1,7 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
 import upload from "../middleware/multer.js"
-import { createCourse, editCourse, getCourseById, getCreatorCourses, getPublishedCourses, removeCourse } from "../controller/courseController.js"
+import { createCourse, createLecture, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getPublishedCourses, removeCourse, removeLecture } from "../controller/courseController.js"
 
 const courseRouter = express.Router()
 
@@ -11,5 +11,13 @@ courseRouter.get("/getcreator",isAuth,getCreatorCourses)
 courseRouter.post("/editcourse/:courseId",isAuth,upload.single("thumbnail"),editCourse)
 courseRouter.get("/getcourse/:courseId",isAuth,getCourseById)
 courseRouter.delete("/remove/:courseId",isAuth,removeCourse)
+
+
+// lecture
+
+courseRouter.post("/createlecture/:courseId",isAuth,createLecture)
+courseRouter.get("/courselecture/:courseId",isAuth,getCourseLecture)
+courseRouter.post("/editlecture/:lectureId",isAuth,upload.single("videoUrl"),editLecture)
+courseRouter.delete("/removelecture/:lectureId",isAuth,removeLecture)
 
 export default courseRouter
